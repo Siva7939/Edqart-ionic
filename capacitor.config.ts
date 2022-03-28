a2596b0c12b835b83df643b4013c510c72b7496e
siva7939 <bsiva507@gmail.com>
2022-03-28 23:58:31 +0530

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'edqart',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
